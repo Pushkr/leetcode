554579f8e971f52efbdc82e289da782d903db1a6
PUSHKAR GUJAR <pushkarvgujar@gmail.com>
2016-10-09 10:09:01 -0400

    Create ex09.py

---
+++ ex09.py
@@ -0,0 +1,30 @@
+'''
+Given two non-negative numbers num1 and num2 represented as string, return the sum of num1 and num2.
+
+Note:
+
+The length of both num1 and num2 is < 5100.
+Both num1 and num2 contains only digits 0-9.
+Both num1 and num2 does not contain any leading zero.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+'''
+
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(char2num(num1) + char2num(num2))
+
+
+def char2num(number):
+    num_dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    num = 0
+    for i, x in enumerate(reversed(list(number))):
+         if i == 0:
+            num = num + num_dict[x]
+         else:
+            num = num + num_dict[x] * (10 ** i)
+    return num
